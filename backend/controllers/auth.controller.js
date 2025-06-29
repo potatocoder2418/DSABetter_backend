6ae0b6f665d18e4966dcacc453fea4b4d1b195b7
potatocoder2418 <cuteamor1824@gmail.com>
2025-06-29 22:16:24 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -78,9 +78,10 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    res.clearCookie("token");
-    return res.status(200).json({
-      message: "Logout successful!",
+   res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
     });
   } catch (error) {
     return res.status(500).json({
